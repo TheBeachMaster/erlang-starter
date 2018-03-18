2bc90e44084f0f2082b71c90c314d97c0ec1d800
TheBeachmaster <otienoken@outlook.com>
2018-03-18 20:42:46 +0300

    Add multiply functionality

--- myapp.erl
+++ myapp.erl
@@ -1,9 +1,12 @@
 -module(myapp).
--export([add/2, greet/0, greet_and_add/1]).
+-export([add/2, greet/0, greet_and_add/1, multiply/2]).
 
 %% Do a simple math
 add(A,B) ->
     A + B.
+
+multiply(U,V) ->
+    U * V.
 
 greet() ->
     io:format("Hello Erlang!~n").
